7f5702eab9f73800b69e0b3f095a03d2b0f52614
AgentD1 <jamparker7@yahoo.com>
2020-05-25 22:23:24 -0400

    Created the Unity project
    Created a basic input system with Unity's new Input System. A few other things

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Player : MonoBehaviour {
+    // Start is called before the first frame update
+    Controls input;
+
+    void Start() {
+        input = new Controls();
+        input.Enable();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Debug.Log(input.Player.Move.ReadValue<Vector2>().x);
+    }
+}
